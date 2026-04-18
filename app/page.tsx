30f1c6fdac20f15d63dbb7985503fba2e94d200d
TonNomGitHub <excellentservice1exls@gmail.com>
2026-04-18 14:41:59 +0100

    mise à jour

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
 
-import { AuthButton } from "@/components/auth-button";
 import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
-import { Suspense } from "react";
 import Boutique from "@/components/boutique";
 import ProductCatalog from "@/components/product-catalog";
-import { getAllProducts } from "@/lib/products";
 
 
 export default function Home() {
