f8608561b710ba084e35c381693b8bc03b9eeb19
TonNomGitHub <excellentservice1exls@gmail.com>
2026-04-18 14:58:36 +0100

    mise à jour

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
-
-import { Hero } from "@/components/hero";
+import Hero from "@/components/hero";
 import Boutique from "@/components/boutique";
 import ProductCatalog from "@/components/product-catalog";
+import { getAllProducts } from "@/lib/products";
 
+export const revalidate = 60; // ISR toutes les 60 secondes
 
-export default function Home() {
+export default async function Home() {
+  const products = await getAllProducts();
+
   return (
-    <main>
-      <body>
-        <Hero />
-        <Boutique />
-        <ProductCatalog
-          title="Nos dernières nouveautés"
-          products={products}
-        />
-      </body>
-    </main>
+    <body>
+      <Hero />
+      <Boutique />
+      <ProductCatalog
+        title="Nos dernières nouveautés"
+        products={products}
+      />
+      
+    </body>
   );
 }
