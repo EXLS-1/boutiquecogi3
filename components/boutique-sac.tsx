c696a1c9ef53107b0d56f3901ad99c93247eca83
EXLS-1 <excellentservice1exls@gmail.com>
2026-04-25 05:30:14 +0100

    feat: add BoutiqueSac component to showcase women's bags collection; remove unused NextLogo component

---
+++ components/boutique-sac.tsx
@@ -0,0 +1,32 @@
+// components/boutique.tsx
+import Image from 'next/image'
+
+export default function BoutiqueSac() {
+  return (
+    <section className="py-20 bg-gray-50" id="boutique">
+  
+        <div className="group relative aspect-4/5 overflow-hidden rounded-2xl bg-gray-200 shadow-md">
+            <Image
+                src="@/Media/pict04.webp"
+                alt="Chaussure Dame"
+                width={400}
+                height={500}
+                className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
+            />
+            <div className="absolute inset-0 bg-linear-to-t from-black/80 via-black/20 to-transparent flex flex-col justify-end p-8 text-white">
+                <h3 className="font-playfair text-2xl font-bold mb-2">
+                SAC DAME
+                </h3>
+                <p className="text-gray-200 mb-6">Accessoires indispensables</p>
+                <a
+                href="#catalogue-chaussure"
+                className="inline-block border border-white px-6 py-2 text-sm font-bold tracking-widest uppercase hover:bg-white hover:text-black transition-colors w-max"
+                >
+                VOIR LA COLLECTION
+                </a>
+            </div>
+        </div>
+
+    </section>
+  )
+}
--- components/boutique.tsx
+++ components/boutique.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import BoutiqueFemme from "./boutique-femme";
 import BoutiqueHomme from './boutique-homme';
 import BoutiqueEnfant from './boutique-enfant';
+import BoutiqueSac from './boutique-sac';
 
 export default function Boutique() {
   return (
@@ -30,27 +31,7 @@ export default function Boutique() {
           <BoutiqueEnfant />
 
             {/* Catégorie : Chaussure */}
-          <div className="group relative aspect-4/5 overflow-hidden rounded-2xl bg-gray-200 shadow-md">
-            <Image
-              src="@/Media/pict04.webp"
-              alt="Chaussure Dame"
-              width={400}
-              height={500}
-              className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
-            />
-            <div className="absolute inset-0 bg-linear-to-t from-black/80 via-black/20 to-transparent flex flex-col justify-end p-8 text-white">
-              <h3 className="font-playfair text-2xl font-bold mb-2">
-                CHAUSSURE DAME
-              </h3>
-              <p className="text-gray-200 mb-6">Confort et élégance</p>
-              <a
-                href="#catalogue-chaussure"
-                className="inline-block border border-white px-6 py-2 text-sm font-bold tracking-widest uppercase hover:bg-white hover:text-black transition-colors w-max"
-              >
-                VOIR LA COLLECTION
-              </a>
-            </div>
-          </div>
+          <BoutiqueSac />
 
           {/* Catégorie : Sac */}
           <div className="group relative aspect-4/5 overflow-hidden rounded-2xl bg-gray-200 shadow-md">
